8190e039720fbe0c2db3c5335d25065e68e225d5
naeunkim1227 <kne0140@daum.net>
2021-10-19 14:18:59 +0900

    21.10.19

--- mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/douzone/mysite/controller/UserController.java
@@ -1,11 +1,16 @@
 package com.douzone.mysite.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.douzone.mysite.service.UserService;
+import com.douzone.mysite.vo.UserVO;
 
 @Controller
 @RequestMapping("/user")
@@ -18,5 +23,51 @@ public class UserController {
 	public String join() {
 		return "user/join";
 	}
+	
+	
+	@RequestMapping(value = "/join" ,method = RequestMethod.POST)
+	public String join(UserVO vo) {
+		userService.join(vo);
+		return "redirect:/user/joinsuccess";
+	}
+	
+	@RequestMapping("/joinsuccess")
+	public String joinsuccess() {
+		return "user/joinsuccess";
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "user/login";
+	}
+	
+	
+	@RequestMapping(value = "/update", method = RequestMethod.GET)
+	public String update(HttpSession session) {
+		return "user/update";
+	}
+	
+	
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(
+			HttpSession session,
+			@RequestParam(value = "email", required = true, defaultValue = "" ) String email, 
+			@RequestParam(value = "password", required = true, defaultValue = "") String password,
+			Model model) {
+		 UserVO userVo = userService.getuser(email,password);
+		 if(userVo == null) { 
+		 model.addAttribute("result", "fail");
+		 }
+
+		 //인증처리
+		 session.setAttribute("authUser", userVo);
+		 
+		return "redirect:/";
+	}
+	
+	
+	
+	
+	
 	
 }
--- mysite03/src/main/java/com/douzone/mysite/service/UserService.java
+++ mysite03/src/main/java/com/douzone/mysite/service/UserService.java
@@ -4,12 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.douzone.mysite.repository.UserRepository;
+import com.douzone.mysite.vo.UserVO;
 
 @Service
 public class UserService {
 	
 	@Autowired
 	private UserRepository userRepository;
+
+	public void join(UserVO vo) {
+		userRepository.insert(vo);
+	}
+
+	public UserVO getuser(Long no) {
+		return userRepository.findByNo(no);
+	}
+	
+	
+	public UserVO getuser(String email, String password) {
+		return userRepository.findbyEmailAndPassword(email, password);
+	}
 	
 	
 	
