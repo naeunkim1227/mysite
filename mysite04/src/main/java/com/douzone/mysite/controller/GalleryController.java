d60185e3d35ea4056e11ce2fab609d8feda6786f
naeunkim1227 <kne0140@daum.net>
2021-10-28 11:33:34 +0900

    21.10.28 mysite04

---
+++ mysite04/src/main/java/com/douzone/mysite/controller/GalleryController.java
@@ -0,0 +1,54 @@
+package com.douzone.mysite.controller;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.douzone.mysite.repository.GalleryRepository;
+import com.douzone.mysite.service.GalleryService;
+import com.douzone.mysite.vo.GalleryVo;
+
+@Controller
+@RequestMapping("/gallery")
+public class GalleryController {
+	
+	@Autowired
+	private GalleryService galleryService;
+	
+	@RequestMapping("")
+	public String index(Model model){
+		
+		List<GalleryVo> list = galleryService.getimage();
+		
+		model.addAttribute("list", list);
+	
+		return "gallery/index";
+	}
+
+	@RequestMapping(value = "/upload" , method = RequestMethod.POST)
+	public String upload(@RequestParam(value = "comments", required = true, defaultValue = "") String comments,
+			@RequestParam(value = "file") MultipartFile multipartFile) {
+	
+		galleryService.upload(multipartFile, comments);
+		
+		return "redirect:/gallery";
+	}
+	
+	
+	@RequestMapping(value = "/delete/{no}")
+	public String delete(@PathVariable("no") Long no) {
+		
+		galleryService.delete(no);
+		
+		return "redirect:/gallery";
+	}
+
+}
---
+++ mysite04/src/main/java/com/douzone/mysite/repository/GuestbookRepository.java
@@ -0,0 +1,52 @@
+package com.douzone.mysite.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.douzone.mysite.exception.GuestbookRepositoryException;
+import com.douzone.mysite.vo.guestbookVO;
+
+
+@Repository
+public class GuestbookRepository {
+	
+	@Autowired
+	private DataSource dataSource;
+	
+	@Autowired
+	private SqlSession sqlsession;
+	
+	Connection conn = null;
+	PreparedStatement pstmt = null;
+	ResultSet rs = null;
+	String sql = null;
+	
+	//insert
+	public void insert(guestbookVO vo) {
+		System.out.println(vo);
+		sqlsession.insert("guestbook.insert", vo);
+	}
+	
+	//select
+	public List<guestbookVO> findall() throws GuestbookRepositoryException {
+		List<guestbookVO> list = sqlsession.selectList("guestbook.findAll");
+		return list;
+	}
+	
+	//delete
+	public void delete(guestbookVO vo) {
+		sqlsession.delete("guestbook.delete", vo);	
+	}
+	
+}
